aa82faa87c0c370ee18d8ba709bf112e35d7cc07
mafutka <mafutka@gmail.com>
2025-06-20 20:56:19 +0300

    Props-deleted

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,14 +2,7 @@ import NoteDetailsClient from './NoteDetails.client';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNoteById } from '../../../lib/api'; 
 
-
-type Params = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function NoteDetailsPage({ params }: Params) {
+export default async function NoteDetailsPage({ params }: { params: { id: string } }) {
   const noteId = params.id; 
   const queryClient = new QueryClient();
 
